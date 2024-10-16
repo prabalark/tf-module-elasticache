b467387b0cb429ab8e5b2bcb79b36a613839d743
prabala <radhakrishnaprabala8@gmail.com>
2024-10-16 09:14:06 +0530

    main.tf vr1

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "main" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "REDIS"
+    description = "elasticache"
     from_port   = var.port
     to_port     = var.port
     protocol    = "tcp"
